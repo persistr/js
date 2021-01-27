94c29ebf4bcd9ebec183a09ac9a15655569a9543
Sasha Jolich <sasha@ctoatlarge.com>
2021-01-26 23:17:07 -0800

    4.0.0
    Matches the Persistr Server v4.0.0 release

--- lib/persistr.d.ts
+++ lib/persistr.d.ts
@@ -1,89 +1,82 @@
-// Type definitions for Persistr 2
-// Project: Persistr
-// Definitions by: Sasa Djolic <https://www.linkedin.com/in/sasadjolic/>
-
 export class Persistr {
-    constructor();
-    account({ environment, credentials }: { environment?: string, credentials: { apikey?: string; }}): Promise<Persistr.Account>;
+  constructor();
+  connect(options?: { environment?: string, credentials?: { apikey: string } | { email: string, password: string }, server?: string }): Promise<Persistr.Account>;
 }
 
 declare namespace Persistr {
-    export interface Account {
-        space(name: string): Space;
-        spaces(): Spaces;
-    }
+  export interface Account {
+    db(name: string): Database;
+    database(name: string): Database;
+    dbs(): Databases;
+    databases(): Databases;
+  }
 
-    export interface Adapter {
-        on(callback: (event: EventObject) => void): Promise<void>;
-    }
+  export interface Annotation {
+    destroy(): Promise<void>;
+    read(): Promise<any>;
+  }
 
-    export interface Annotation {
-        destroy(): Promise<void>;
-        export(): Promise<void>;
-        read(): Promise<any>;
-    }
+  export interface Database {
+    create(): Promise<Database>;
+    clone(to: string): Promise<void>;
+    destroy(): Promise<void>;
+    rename(to: string): Promise<Database>;
+    grant(access: { role: string, email: string }): Promise<Database>;
+    revoke(access: { role: string, email: string }): Promise<Database>;
+    namespace(name: string): Namespace;
+    namespaces(): Namespaces;
+  }
 
-    export interface Domain {
-        create(): Promise<Domain>;
-        destroy(): Promise<void>;
-        export(): Promise<Domain>;
-        truncate(): Promise<void>;
-        rename(to: string): Promise<Domain>;
-        stream(name: string): Stream;
-        streams(): Streams;
-        events(options?: { types?: string | string[], from?: string, to?: string, after?: string, until?: string }): Events;
-    }
+  export interface Databases {
+    each(callback: (db: string) => void): Promise<void>;
+    all(): Promise<string[]>;
+  }
 
-    export interface Domains {
-        each(): Promise<void>;
-    }
+  export interface Event {
+    destroy(): Promise<void>;
+    read(): Promise<any>;
+  }
 
-    export interface Event {
-        destroy(): Promise<void>;
-        export(): Promise<void>;
-        read(): Promise<any>;
-    }
+  type EventObject = {
+      data: any,
+      meta?: any
+  }
 
-    type EventObject = {
-        data: any,
-        meta?: any
-    }
+  export interface Events {
+    each(callback: (event: EventObject, subscription?: Subscription) => void): Promise<void>;
+    write(events: string | EventObject | Array<EventObject>, data?: any): Promise<number>;
+  }
 
-    export interface Events {
-        each(callback: (event: EventObject, subscription?: Subscription) => void): Promise<void>;
-        write(events: EventObject | Array<EventObject>): number;
-        via(options: any): Adapter;
-    }
+  export interface Namespace {
+    create(): Promise<Namespace>;
+    destroy(): Promise<void>;
+    truncate(): Promise<void>;
+    rename(to: string): Promise<Namespace>;
+    stream(name: string): Stream;
+    streams(): Streams;
+    events(options?: string | { types?: string | string[], from?: string, to?: string, after?: string, until?: string, limit?: number }): Events;
+  }
 
-    export interface Space {
-        create(): Promise<Space>;
-        destroy(): Promise<void>;
-        export(): Promise<Space>;
-        rename(to: string): Promise<Space>;
-        domain(name: string): Domain;
-        domains(): Domains;
-    }
+  export interface Namespaces {
+    each(callback: (ns: string) => void): Promise<void>;
+  }
 
-    export interface Spaces {
-        each(): Promise<void>;
-    }
+  export interface Stream {
+    annotate(annotation: any): Promise<void>;
+    annotation(): Annotation;
+    event(id: string): Event;
+    events(options?: string | { types?: string | string[], from?: string, to?: string, after?: string, until?: string, limit?: number }): Events;
+    destroy(): Promise<void>;
+  }
 
-    export interface Stream {
-        annotate(annotation: any): Promise<void>;
-        annotation(): Annotation;
-        event(id: string): Event;
-        events(options?: { types?: string | string[], from?: string, to?: string, after?: string, until?: string }): Events;
-        destroy(): Promise<void>;
-        export(): Promise<void>;
-    }
+  export interface Streams {
+    stream(name: string): Stream;
+    each(callback: (stream: Stream) => void): Promise<void>;
+  }
 
-    export interface Streams {
-        each(): Promise<void>;
-    }
-
-    export interface Subscription {
-        cancel(): Promise<void>;
-    }
+  export interface Subscription {
+    cancel(): Promise<void>;
+  }
 }
 
 export declare var persistr: Persistr;
